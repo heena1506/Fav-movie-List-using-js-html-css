97febe8cd951e321c04e2115bf473b8fdc228043
heena1506 <94212107+heena1506@users.noreply.github.com>
2022-08-23 23:52:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,126 @@
-//*****************display form data in table******************/
+showmovie();
+//******display form data in table*******/
 function displayForm() {
-   let obj = document.getElementById("formtab");
-  obj.style.display == "none" ? obj.style.display = "block" : obj.style.display = "none";
-  }
-}
-//*******************increment the rating*********************/
-function increment(id) {
-  let x = document.getElementById(id).innerText;
-  if (x < 5) {
-    document.getElementById(id).innerText =
-      parseInt(document.getElementById(id).innerText) + 1;
-  }
-  
-}
-//*****************decrement the rating value*******************/
-function decrement(id) {
-  let x = document.getElementById(id).innerText;
-  if (x > 1) {
-    document.getElementById(id).innerText =
-      parseInt(document.getElementById(id).innerText) - 1;
-  }
-  
-}
-//*****************delete the item from the table***************/
-function deleteElement(id) {
-  document.getElementById(id).remove();
-}
-//**************add itmes into table*********************/
-function addItem() {
-  let titledata = document.getElementById("title").value;
-  let imagedata = document.getElementById("image").value;
-  let ratingdata = document.getElementById("rating").value;
+    let obj = document.getElementById("formtab");
+   obj.style.display == "none" ? obj.style.display = "block" : obj.style.display = "none";
 
-  let mainobj = document.getElementById("tabledata");
-
-  if (titledata != "" && imagedata != "" && ratingdata != "") {
-    let element = document.createElement("tr");
-    let trid = getRandom();
-    element.id = trid;
-    let title = document.createElement("td");
-    let imagetd = document.createElement("td");
-    let image = document.createElement("img");
-    image.src = imagedata;
-    imagetd.appendChild(image);
-    let rating = document.createElement("td");
-    let update = document.createElement("td");
-
-    title.innerHTML = titledata;
-
-    let counterid = parseInt(getRandom());
-
-    rating.innerHTML = `<button onclick="increment(this.value)" value="${counterid}">👍</button>
-                        <span id="${counterid}">${ratingdata}</span><button onclick="decrement(this.value)" value="${counterid}">👎</button>`;
-
-    update.innerHTML = `<button onclick="deleteElement(this.value)" value="${trid}"> X </button>`;
-    //*******************append the element**********************/
-    element.appendChild(imagetd);
-    element.appendChild(title);
-    element.appendChild(rating);
-    element.appendChild(update);
-    mainobj.appendChild(element);
+   }
+ 
+ //******increment the rating********/
+ function increment(id) {
+    let movie=localStorage.getItem("movie");
+    movieObj= JSON.parse(movie);
+    let rating=movieObj[id].rat;
+    if(rating<5){
+        let obj={
+            Title:movieObj[id].Title,
+            Img:movieObj[id].Img,
+            rat:Number(rating)+1
+        }
+        movieObj.splice(id,1,obj)
+        localStorage.setItem("movie",JSON.stringify(movieObj))
+    
+        showmovie();
+    }
+   
+ }
+ //******decrement the rating value******/
+ function decrement(id) {
+    let movie=localStorage.getItem("movie");
+    movieObj= JSON.parse(movie);
+    let rating=movieObj[id].rat;
+    if(rating>0){
+        let obj={
+            Title:movieObj[id].Title,
+            Img:movieObj[id].Img,
+            rat:Number(rating)-1
+        }
+        movieObj.splice(id,1,obj)
+        localStorage.setItem("movie",JSON.stringify(movieObj))
+    
+        showmovie();
+    }
+ }
+ //******delete the item from the table******/
+ function deleteElement(id) {
+    //console.log(id);
+    let movie=localStorage.getItem("movie");
+    if(movie==null){
+        movieObj=[];
+    }
+    else{
+        movieObj= JSON.parse(movie);
+    }
+    movieObj.splice(id,1);
+    localStorage.setItem("movie",JSON.stringify(movieObj)); 
+    showmovie();
+ }
+ //*****add itmes into table********/
+ function addItem() {
+   let titledata = document.getElementById("title").value;
+   let imagedata = document.getElementById("image").value;
+   let ratingdata = document.getElementById("rating").value;
+   let movie=localStorage.getItem("movie");
+    if(movie==null){
+        movieObj=[];
+    }
+    else{
+        movieObj= JSON.parse(movie);
+    }
+    let obj={
+        Title:titledata,
+        Img:imagedata,
+        rat:ratingdata
+    }
+    movieObj.push(obj);
+    localStorage.setItem("movie",JSON.stringify(movieObj));
+    showmovie();
     document.getElementById("formtab").style.display = "none";
-    document.getElementById("title").value = "";
-    document.getElementById("image").value = "";
-    document.getElementById("rating").value = "";
-  } else {
-    alert("OOPS! Please fill in all the fields.");
-  }
-}
-//***********random code genaretor*****************/
-function getRandom() {
-  let x = Math.random();
-  x = Math.floor(x * 10000);
-  return x;
+    document.getElementById("title").value="";
+    document.getElementById("image").value="";
+    document.getElementById("rating").value="";
+ }
+
+
+
+ function showmovie(){
+    let movie=localStorage.getItem("movie");
+    if(movie==null){
+        movieObj=[];
+    }
+    else{
+        movieObj= JSON.parse(movie);
+    }
+    let html=` <tr>
+    <th>Image</th>
+    <th>Name</th>
+    <th>Rating</th>
+    <th>Update</th>
+  </tr>`;
+    movieObj.forEach(function(element,index){
+        html +=`<tr id=${index}>
+        <td>
+          <img
+            src='${element.Img}'  
+          />
+        </td>
+        <td>${element.Title}</td>
+        <td>
+          <button onclick="increment(${index})" >
+            👍
+          </button>
+          <span id="counter2">${element.rat}</span
+          ><button onclick="decrement(${index})" >
+            👎
+          </button>
+        </td>
+        <td>
+          <button  onclick="deleteElement(${index})" >
+             <i class="fa fa-trash"></i>
+          </button>
+        </td>
+      </tr>`
+    });
+    let mainobj = document.getElementById("tabledata");
+    mainobj.innerHTML=html;
 }
